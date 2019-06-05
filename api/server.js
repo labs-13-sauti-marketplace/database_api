6d85f924555c8f62eb65c2d9aa2814116763f420
Wei Du <wei.du.2015@marshall.usc.edu>
2019-06-05 17:04:44 -0400

    adjust server files and server is running, also add server catch all response

--- api/server.js
+++ api/server.js
@@ -1,10 +1,16 @@
 const express  = require('express');
-const server   = express();
+const helmet = require('helmet');
+
+const server = express();
 
 server.use(express.json());
-
+server.use(helmet());
 server.get('/', (req, res) => {
   res.send('server is up');
 });
+
+server.use((req, res, next) => {
+  res.status(404).json({message:'in server route'})
+})
 
 module.exports = server;
