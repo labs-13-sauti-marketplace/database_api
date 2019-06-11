9332659c2b8eb3bbaed4386986d6766dc8e4fa42
Wei Du <wei.du.2015@marshall.usc.edu>
2019-06-11 16:10:54 -0400

    adj server files

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 const express  = require('express');
 const helmet = require('helmet');
-
+const db = require('../data/dbConfig')
 const server = express();
 
 server.use(express.json());
@@ -8,7 +8,23 @@ server.use(helmet());
 server.get('/', (req, res) => {
   res.send('server is up');
 });
-
+server.post('*', async (req, res) => {
+  let { sessionId, serviceCode, phoneNumber, text } = req.body;
+  let response = '';
+  switch (text) {
+    case '':
+      let sql = `SELECT name FROM countries`;
+      try {
+        const names = await db.raw(sql);
+        response = `END countries ${names[0].name}`
+      }catch(err){
+        console.log(err)
+      }
+      break;
+    default:
+      response = 'bad request';    
+  } 
+})
 server.use((req, res, next) => {
   res.status(404).json({message:'in server route'})
 })
