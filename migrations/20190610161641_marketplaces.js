198265c22e413e93d6c03f16559a4c86bf821aab
nevets615 <surj316@yahoo.com>
2019-06-12 16:26:20 -0400

    created user and sessions tables, seeded countries, marketplace, category and products tables

--- migrations/20190610161641_marketplaces.js
+++ migrations/20190610161641_marketplaces.js
@@ -1,20 +1,12 @@
-
-exports.up = function (knex, Promise) {
-  return knex.schema.createTable('marketplaces', tbl => {
-    tbl.increments()
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable("marketplaces", tbl => {
+    tbl.increments();
     tbl
-      .string('name', 128)
+      .string("name", 128)
       .notNullable()
-      .unique()
-    tbl
-      .integer('country_id')
-  .unsigned()
-      .references('id')
-      .inTable('countries')
-      .onDelete('CASCADE')
-      .onUpdate('CASCADE')
-  })
-}
-exports.down = function (knex, Promise) {
-  return knex.schema.dropTableIfExists('marketplaces')
+      .unique();
+  });
+};
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists("marketplaces");
 };
