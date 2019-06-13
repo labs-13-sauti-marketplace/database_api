d944caba06476ca66c35a67d5a0ff1cb1457e1cc
ohheyimjen <jendority@gmail.com>
2019-06-13 16:24:00 -0700

    moved migrations and seeds into data folder

---
+++ data/seeds/002-marketplaces.js
@@ -0,0 +1,12 @@
+exports.seed = function (knex, Promise) {
+  return knex("marketplaces").insert([
+    { name: "Busia" }, // 1
+    { name: "Tororo" }, // 2
+    { name: "Mbale" }, // 3
+    { name: "Eldoret" }, // 4
+    { name: "Kisumu" }, // 5
+    { name: "Soroti" }, // 6
+    { name: "Owino" }, // 7
+    { name: "Kampala" } // 8
+  ]);
+};
