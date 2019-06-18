27380d0cdcb8718396d4b2c5d77c534d9c207552
cammac <camm@rocktechcomputer.com>
2019-06-18 13:26:26 -0700

    created 8 tables for product categories

---
+++ data/migrations/20190618132022_fruits.js
@@ -0,0 +1,21 @@
+
+exports.up = function (knex, Promise) {
+  return knex.schema.createTable('fruits', tbl => {
+    tbl.increments()
+    tbl
+      .string('name', 128)
+      .notNullable()
+    tbl
+      .integer('category_id')
+      .unsigned()
+      .references('id')
+      .inTable('categories')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE')
+  })
+};
+
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTableIfExists('fruits')
+};
+
