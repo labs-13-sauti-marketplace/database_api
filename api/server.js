1ce29bca72754b3925dbb3b38f0aa2988c0b4d0a
Wei Du <wei.du.2015@marshall.usc.edu>
2019-06-18 18:35:09 -0400

    change files

--- api/server.js
+++ api/server.js
@@ -12,10 +12,6 @@ server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use('*', router);
 
-// setInterval(function () {
-//   https.get('https://sauti-marketplace.herokuapp.com/');
-// }, 300000);
-
 server.get('/', (req, res) => {
   res.send('server is up');
 });
