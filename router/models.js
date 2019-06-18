e0b61fb4af41a52f63da4164d4a11ab3ddab3c95
cammac <camm@rocktechcomputer.com>
2019-06-18 14:56:27 -0700

    clean up models

--- router/models.js
+++ router/models.js
@@ -2,37 +2,28 @@ const db = require("../data/dbConfig");
 
 module.exports = {
   findPrice,
-<<<<<<< HEAD
-<<<<<<< HEAD
   addProduct,
-  
-=======
-  get
->>>>>>> 418a25317f54fb011715cbc1b495d963efb0da7f
-=======
+  get,
   getMarkets,
   getCat,
   getProducts
->>>>>>> 60fc1eae35b2bd9fc73cc5313c6a450405ff07c7
 };
 
 function findPrice(name, product) {
   return db("markets")
     .select("price")
     .where({ name: name, product: product });
-
 }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 async function addProduct(product) {
   const [id] = await db('products').insert(product)
   // return 
-=======
+}
+
 function get() {
   return db('marketplaces')
->>>>>>> 418a25317f54fb011715cbc1b495d963efb0da7f
-=======
+}
+
 function getMarkets() {
   return db('marketplaces')
 }
@@ -43,5 +34,4 @@ function getCat() {
 
 function getProducts() {
   return db('products')
->>>>>>> 60fc1eae35b2bd9fc73cc5313c6a450405ff07c7
 }
