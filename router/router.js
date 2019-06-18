6de82cfc07d6138cd37da7521fc072bc1b27d287
ohheyimjen <jendority@gmail.com>
2019-06-18 13:10:37 -0700

    added a market to router

--- router/router.js
+++ router/router.js
@@ -4,50 +4,6 @@ const UssdMenu = require('ussd-menu-builder')
 const models = require("./models");
 const menu = new UssdMenu()
 
-<<<<<<< HEAD
-// add some type of middleware, before async, to verify session
-router.post("*", async (req, res) => {
-  let { sessionId, serviceCode, phoneNumber, text } = req.body;
-  let response = "";
-  switch (text) {
-    case "":
-      response =
-        "CON Welcome to Sauti Marketplace. Please choose an option \n 1. Buy \n 2. Sell";
-      break;
-    case "1":
-      response =
-        "CON Choose your marketplace \n 1. Bujumbura \n 2. Tororo \n 3. Mbale \n 4. Eldoret \n 5. Kisumu \n 6. Soroti \n 7. Ownio \n 8. Kampala";
-      break;
-    case "2":
-      response = "END Post here eventually, thank you!";
-      break;
-    case "1*1":
-      response =
-        "CON Choose your category \n 1. Animal Products \n 2. Beans \n 3. Cereals \n 4. Fruits \n 5. Roots & Tubers \n 6. Seeds & Nuts \n 7. Vegetables \n 8. Other";
-      break;
-    case "1*1*1":
-      response = "CON Choose your product...coming soon!";
-      break;
-    case "1*1*1*1":
-      "END Post here eventually, thank you!";
-      break;
-      
-      try {
-        const results = await models.findPrice("Bujumbura", "white eggs");
-        let newPrice = [];
-        results.forEach(function(cake) {
-          newPrice.push(cake.price);
-        });
-        newPrice.toString();
-
-        response = `END Current prices for \n White Eggs ${newPrice}`;
-        // need to store/send data 
-      } catch (error) {
-        console.log(error);
-        // do stuff with error
-      }
-=======
->>>>>>> 418a25317f54fb011715cbc1b495d963efb0da7f
 
 const bodyParser = require('body-parser')
 const UssdMenu = require('ussd-menu-builder');
@@ -81,7 +37,7 @@ menu.state('goodbye', {
 
 menu.state('position', {
   run: () => {
-    menu.con(`\n1. buyer \n2. seller`)
+    menu.con(`\n1. Animal Products \n2. Beans `)
   },
   next: {
     '1': 'buyer',
@@ -101,16 +57,26 @@ menu.state('buyer', {
     })}` 
   },
   next: {
-    '1':'Bujumbura', 
+    '1':'Busia', 
     '2':'Tororo', 
     '3':'Mbale', 
     '4':'Eldoret', 
     '5':'Kisumu', 
     '6':'Soroti', 
-    '7':'Ownio',
+    '7':'Bungoma',
     '8':'Kampala'
   }
 });
+
+menu.state('Eldoret', {
+  run: () => {
+    menu.con(`\n1. Animal Products \n2. Beans \n3. Cereals \n4. Fruits \n5. Other \n6. Roots & Tubers \n7. Seeds & Nuts \n8. Vegetables)
+  },
+  next: {
+    '1': 'buyer',
+    '2': 'seller'
+  }
+})
 
 
 menu.on('error', err => {
