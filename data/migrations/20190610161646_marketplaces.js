31b86f95851204cf736346002a08baa29c7b60d7
Wei Du <wei.du.2015@marshall.usc.edu>
2019-06-24 17:55:02 -0400

    pull table from master

--- data/migrations/20190610161646_marketplaces.js
+++ data/migrations/20190610161646_marketplaces.js
@@ -1,21 +1,20 @@
-
 exports.up = function (knex, Promise) {
-  return knex.schema.createTable('marketplaces', tbl => {
-    tbl.increments()
-    tbl
-      .string('name', 128)
-      .notNullable()
-      .unique()
-    tbl
-      .integer('country_id')
-      .unsigned()
-      .references('id')
-      .inTable('countries')
-      .onDelete("CASCADE")
-      .onUpdate('CASCADE')
-  })
-};
-
-exports.down = function (knex, Promise) {
-  return knex.schema.dropTableIfExists('marketplaces')
-};
+    return knex.schema.createTable('marketplaces', tbl => {
+      tbl.increments()
+      tbl
+        .string('name', 128)
+        .notNullable()
+        .unique()
+      tbl
+        .integer('country_id')
+        .unsigned()
+        .references('id')
+        .inTable('countries')
+        .onDelete("CASCADE")
+        .onUpdate('CASCADE')
+    })
+  };
+  
+  exports.down = function (knex, Promise) {
+    return knex.schema.dropTableIfExists('marketplaces')
+  };
