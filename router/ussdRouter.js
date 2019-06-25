d57ee888681cc4f329953a459c06eb795cb921cd
cammac <camm@rocktechcomputer.com>
2019-06-25 12:53:43 -0700

    test

--- router/ussdRouter.js
+++ router/ussdRouter.js
@@ -27,6 +27,32 @@ async function products() {
   return result
 }
 
+// let sessions = {};
+menu.sessionConfig({
+  start: (sessionId, callback) => {
+    // initialize current session if it doesn't exist
+    // this is called by menu.run()
+    if (!(sessionId in sessions)) sessions[sessionId] = {};
+    callback();
+  },
+  end: (sessionId, callback) => {
+    // clear current session
+    // this is called by menu.end()
+    delete sessions[sessionId];
+    callback();
+  },
+  set: (sessionId, key, value, callback) => {
+    // store key-value pair in current session
+    sessions[sessionId][key] = value;
+    callback();
+  },
+  get: (sessionId, key, callback) => {
+    // retrieve value by key in current session
+    let value = sessions[sessionId][key];
+    callback(null, value);
+  }
+});
+
 // setting initial state of menu
 menu.startState({
   run: () => {
