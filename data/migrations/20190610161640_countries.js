0d569dbe7638d45cffcf795a0a4efd920c7081fa
cammac <camm@rocktechcomputer.com>
2019-06-25 09:50:01 -0700

    added primary tag

--- data/migrations/20190610161640_countries.js
+++ data/migrations/20190610161640_countries.js
@@ -1,7 +1,7 @@
 
 exports.up = function (knex, Promise) {
   return knex.schema.createTable('countries', tbl => {
-    tbl.increments()
+    tbl.increments().primary()
     tbl
       .string('name', 128)
       .notNullable()
