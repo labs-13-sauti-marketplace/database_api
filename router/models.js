ec7f8213c0d2e28cfbaf99a16a58337c8cd2a37e
cammac <camm@rocktechcomputer.com>
2019-06-26 14:20:20 -0700

    test

--- router/models.js
+++ router/models.js
@@ -28,7 +28,8 @@ function get() {
 }
 
 function getProductByMarketAndCatId(categoryId) {
-  return db("products").where({ category_id: categoryId });
+  return db("products")
+    .where({ 'category_id': categoryId });
 }
 
 function getMarkets() {
