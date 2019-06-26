1f34c9bcfda6965798d9a00121985d4f11b1a5af
cammac <camm@rocktechcomputer.com>
2019-06-26 14:03:46 -0700

    test

--- router/ussdRouter.js
+++ router/ussdRouter.js
@@ -120,9 +120,9 @@ menu.state('market', {
 
 menu.state("category", {
   run: () => {
-    // let marketplaceId = menu.val;
+    let marketplaceId = menu.val;
     console.log("CATEGORY SESSION STORAGE", sessionStore)
-    console.log("CAT STORAGE ", marketplaceId)
+    // console.log("CAT STORAGE ", marketplaceId)
     console.log("CATEGORY()")
     categories().then(res => {
       let lol = [];
