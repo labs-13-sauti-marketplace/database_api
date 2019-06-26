3b99486608d4831881fcf5661c61ef2952575c8b
Wei Du <wei.du.2015@marshall.usc.edu>
2019-06-26 13:17:22 -0400

    adj

--- router/ussdRouter.js
+++ router/ussdRouter.js
@@ -98,44 +98,6 @@ menu.state("done", {
 })
 
 
-menu.state("Test", {
-  run: () => {
-    menu.end("You made it!");
-  }
-});
-
-menu.state("postForSale", {
-  run: () => {
-    menu.con("Enter a country:");
-  },
-  next: {
-    "*[a-zA-Z]+": "addCountry"
-  }
-});
-
-// nesting states
-menu.state("addCountry", {
-  run: () => {
-    // use menu.val to access user input value
-    let country = menu.val;
-    const product = {
-      country: country,
-      market: "market",
-      product: "product",
-      price: "price"
-    };
-    db("products")
-      .insert(product)
-      .then(res => {
-        menu.end("Country added successfully!");
-      })
-      .catch(err => {
-        menu.end("Fail");
-      });
-  }
-});
-
-
 router.post('*', (req, res) => {
   let args = {
       phoneNumber: req.body.phoneNumber,
