f98845ad4ac5facac456e7db694739f969917bc7
Wei Du <wei.du.2015@marshall.usc.edu>
2019-06-27 18:29:14 -0400

    adj

--- router/ussdRouter.js
+++ router/ussdRouter.js
@@ -106,7 +106,7 @@ menu.state('buyerCountry', {
     })
       .catch(err => {
         console.log(err)
-        menu.con('start')
+        menu.goStart('start')
       })
   },
   next: {
@@ -139,7 +139,7 @@ menu.state('buyerMarket', {
     })
       .catch(err => {
         console.log(err)
-        menu.con('start')
+        menu.goStart('start')
       })
 
   },
