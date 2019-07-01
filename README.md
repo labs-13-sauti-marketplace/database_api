1d1ef0def54f1117ca91d4990eb4d9018ff246e7
nevets615 <surj316@yahoo.com>
2019-07-01 17:12:01 -0400

    added end points for products and country

--- README.md
+++ README.md
@@ -30,14 +30,60 @@ make this software better and more simple, feel free to fork this repo and code 
 
 NodeJs / Express / USSD / Postgres - for more specifications please check our `package.json` file 
 
-There is a Front-End but it is mostly just for testing our API 
+The webrouter.js is a backend API that can be used to build a front end application with functional end points. 
 
 
 
 
 | Method | Endpoint                                    | Access Control | Description                                  |
 | ------ | --------------------------------------------| -------------- | -------------------------------------------- |
-| GET    | `//sauti-marketplace.herokuapp.com/markets` | all markets      | Returns the information for markets. |
+| GET | 
+
+`//sauti-marketplace.herokuapp.com/categories` | all categories      | Returns the information for categories. |
+
+`//sauti-marketplace.herokuapp.com/countries` | all countries      | Returns the information for countries. |
+
+`//sauti-marketplace.herokuapp.com/sessions` | all sessions      | Returns the information for sessions. |
+
+`//sauti-marketplace.herokuapp.com/country/:id` | all country/:id      | Returns the information for country/:id. |
+
+`//sauti-marketplace.herokuapp.com/products/:id` | all products/:id      | Returns the information for products/:id. |
+
+| POST |
+
+`//sauti-marketplace.herokuapp.com/addmarket` | 
+| Added a new marketplace. |
+
+`//sauti-marketplace.herokuapp.com/addcountry` | 
+| Added a new country. |
+
+`//sauti-marketplace.herokuapp.com/addsessions` | 
+| Added a new session. |
+
+`//sauti-marketplace.herokuapp.com/addcategories` | 
+| Added a new category. |
+
+| Delete |
+
+`//sauti-marketplace.herokuapp.com/deletemarket/:id` | 
+| Delete a marketplace. |
+
+`//sauti-marketplace.herokuapp.com/deletecountry/:id` | 
+| Delete a country. |
+
+`//sauti-marketplace.herokuapp.com/deletesession/:id` | 
+| Delete a session. |
+
+| Update |
+
+`//sauti-marketplace.herokuapp.com/updatecountry/:id` | 
+| update a country. |
+
+`//sauti-marketplace.herokuapp.com/updatesession/:id` | 
+| update a session. |
+
+
+
 
 
 ## Authors 
@@ -50,9 +96,9 @@ There is a Front-End but it is mostly just for testing our API
 
 *Sarah Riley* 
 
-*Brad Cantrell* 
-
 *Oscar Ortiz* 
+
+*Wei Du*
 
 
 
--- router/webrouter.js
+++ router/webrouter.js
@@ -14,29 +14,61 @@ webRouter.get("/categories", async (req, res) => {
   }
 });
 
+// //-----------------------------------------------
+
 webRouter.get("/country/:id", (req, res) => {
   const { id } = req.params;
-  const country = [];
+  const country = "";
+  const markets = [];
   db("countries")
     .where("id", id)
-    .then(count => {
-      country.push(count[0]);
+    .then(coun => {
+      country = coun[0].name;
     })
     .then(
       db("marketplaces")
         .where("country_id", id)
         .then(marketplaces => {
-          country[0].marketplaces = marketplaces;
-          
+          markets = marketplaces;
         })
         .then(() => {
-          res.json(country);
+          res.json(country, markets);
         })
     )
     .catch(err => {
       res.status(500).json({ err: "err" });
     });
 });
+
+// //-----------------------------------------------
+
+webRouter.get("/products/:id", (req, res) => {
+  const { marketplace_id, category_id } = req.body;
+
+  db("products")
+    .where({ marketplace_id: marketplace_id, category_id: category_id })
+
+    .then(product => {
+      res.json(product);
+    })
+
+    .catch(err => {
+      res.status(500).json({ err: "err" });
+    });
+});
+
+// //-----------------------------------------------
+
+webRouter.get("/countries", async (req, res) => {
+  try {
+    let result = await db("countries");
+    res.status(200).json(result);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// //-----------------------------------------------
 
 webRouter.post("/addmarket", (req, res) => {
   console.log("we are trying to add a market");
@@ -59,28 +91,12 @@ async function addPosts(post) {
   return `New Post ID: ${post.name} : Added :)`;
 }
 
-webRouter.get("/products/:id", async (req, res) => {
-  try {
-    let id = req.params.id;
-    let result = await db.getProductByMarketAndCatId(id);
-    res.status(200).json(result);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+// //-----------------------------------------------
 
-webRouter.get("/countries", async (req, res) => {
-  try {
-    let result = await db("countries");
-    res.status(200).json(result);
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-webRouter.post("/addcountry", (req, res) => {
-  console.log("we are trying to add a market");
+webRouter.post("/addcategories", (req, res) => {
+  console.log("we are trying to add a categories");
   let post = req.body;
-  addPost(post)
+  addcategoriesPosts(post)
     .then(saved => {
       res.status(201).json(saved);
     })
@@ -89,7 +105,49 @@ webRouter.post("/addcountry", (req, res) => {
     });
 });
 
-async function addPost(post) {
+async function addcategoriesPosts(post) {
+  console.log("before");
+  const func = await db("categoriesplaces")
+    .insert(post)
+    .where({ category: categories });
+  console.log("after");
+  return `New Post ID: ${post.name} : Added :)`;
+}
+
+// //-----------------------------------------------
+
+webRouter.delete("/deletemarket/:id", (req, res) => {
+  let deleted = req.params.id;
+  db("marketplaces")
+    .where({ id: deleted })
+    .del()
+    .then(gone => {
+      if (!gone) {
+        res.send("marketplace does not exist");
+      } else {
+        res.status(402).json({ message: "success" });
+      }
+    })
+    .catch(error => {
+      res.status(501).json({ message: "Failed" });
+    });
+});
+
+// //-----------------------------------------------
+
+webRouter.post("/addcountry", (req, res) => {
+  console.log("we are trying to add a country");
+  let post = req.body;
+  addCountryPost(post)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(({ message }) => {
+      res.status(503).json({ message });
+    });
+});
+
+async function addCountryPost(post) {
   console.log("before");
   const func = await db("countries")
     .insert(post)
@@ -97,6 +155,8 @@ async function addPost(post) {
   console.log("after");
   return `New Post ID: ${post.name} : Added :)`;
 }
+
+// //-----------------------------------------------
 
 webRouter.delete("/deletecountry/:id", (req, res) => {
   let deleted = req.params.id;
@@ -114,6 +174,8 @@ webRouter.delete("/deletecountry/:id", (req, res) => {
       res.status(501).json({ message: "Failed" });
     });
 });
+
+// //-----------------------------------------------
 
 webRouter.put("/updatecountry/:id", (req, res) => {
   let updatedId = req.params.id;
@@ -136,6 +198,8 @@ webRouter.put("/updatecountry/:id", (req, res) => {
     });
 });
 
+// //-----------------------------------------------
+
 webRouter.get("/sessions", async (req, res) => {
   try {
     let result = await db("sessions");
@@ -144,6 +208,8 @@ webRouter.get("/sessions", async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+// //-----------------------------------------------
 
 webRouter.post("/addsessions", (req, res) => {
   console.log("we are trying to add a sessions");
@@ -165,6 +231,8 @@ async function addPost(post) {
   console.log("after");
   return `New Post ID: ${post.name} : Added :)`;
 }
+
+// //-----------------------------------------------
 
 webRouter.delete("/deletesession/:id", (req, res) => {
   let deleted = req.params.id;
